63c7300cf5c49325aae1adc1dc2a069bf87f96ab
ptrekowski <rekowskipault@gmail.com>
2016-03-21 11:31:17 -0400

    Finish home page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+
+	# Returns the full title on a per-page basis.
+	def full_title(page_title)
+
+		base_title = "Home Metrics | Duct and Blower Door Testing - Serving RI And CT"
+		if page_title.empty?
+			return base_title
+		else
+			return page_title
+		end
+	end
+
 end
