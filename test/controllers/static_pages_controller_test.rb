9a6d3d7d661d9b8b4b2de8806c5b36e722ab7dca
ptrekowski <rekowskipault@gmail.com>
2016-03-20 21:36:02 -0400

    Add dynamic titles

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,6 +4,7 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
+    assert_select "title", "Home Metrics | Duct and Blower Door Testing - Serving RI And CT"
   end
 
   test "should get help" do
@@ -11,4 +12,8 @@ class StaticPagesControllerTest < ActionController::TestCase
     assert_response :success
   end
 
+  test "should get about" do
+  	get :about
+  	assert_response :success
+  end
 end
